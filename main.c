5df6f0722946aa5840d9ded8e18851b7d84b8acb
AnaduCG <godzionwin@gmail.com>
2023-06-22 05:36:20 +0100

    Added: proper file reading and handling

--- main.c
+++ main.c
@@ -8,44 +8,34 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd, i = 0;
 	unsigned int line = 1;
-	char buf[1], str[1024];
-	ssize_t r;
+	char buffer[256];
+	FILE *file;
 	stack_t *stack = NULL;
 
 	if (argc != 2)
 		_error("USAGE: monty file\n");
 	/* opening file */
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
+	file = fopen(argv[1], "r");
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	/* reading from file */
-	while ((r = read(fd, buf, 1)) > 0)
+	/* Read lines from the file */
+	while (fgets(buffer, sizeof(buffer), file) != NULL)
 	{
-		/* checking for a new line in file */
-		if (buf[0] == '\n')
+		if (ferror(file))/* Handling read error */
 		{
-			str[i] = '\0';
-			_to_arr(str, line, &stack);
-			line++;
-			i = 0;
+			_error("Error reading file\n");
+			fclose(file);
+			exit(EXIT_FAILURE);
 		}
-		else
-		{
-			str[i] = buf[0];
-			i++;
-		}
+		_to_arr(buffer, line, &stack);
+		line++;
 	}
-	if (r < 0)
-	{
-		close(fd);
-		_error("Error reading file\n");
-	}
-	close(fd);
+
+	fclose(file);
 	free_stack(&stack);
 	return (0);
 }
