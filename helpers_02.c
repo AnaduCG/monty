cc39f733cfc3a6fb6a06b1626f319df3f92c3584
AnaduCG <godzionwin@gmail.com>
2023-06-22 05:55:16 +0100

    Added: documentationf for _is_int() and _strdup() functions

--- helpers_02.c
+++ helpers_02.c
@@ -13,7 +13,13 @@ int add(stack_t **stack)
 	pop(stack);
 	return (1);
 }
-
+/**
+ *_is_int - function that checks if a string can be converted to a valid
+ *		integer
+ *@str:  string to be checked
+ *Return: returns a pointer to the duplicate string and NULL on error
+ *
+ */
 char *_is_int(char *str)
 {
 	int i, n = 0, len = 0;
@@ -36,4 +42,26 @@ char *_is_int(char *str)
 		return (NULL);
 	}
 	return (str);
+}
+/**
+ *_strdup - function that create a string duplicate
+ *@str:  string to be duplicated
+ *Return: returns a pointer to the duplicate string and NULL on error
+ *
+ */
+char *_strdup(const char *str)
+{
+	size_t len = 0;
+	char *new_str = NULL;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = strlen(str) + 1;
+	new_str = malloc(len);
+
+	if (new_str != NULL)
+		memcpy(new_str, str, len);
+
+	return (new_str);
 }
