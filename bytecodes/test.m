5b20996f523dbd8a6f4f10bb5c62f31019f6b0fd
AnaduCG <godzionwin@gmail.com>
2023-06-23 00:12:37 +0100

    Fixed: branch merge and error fixing

--- bytecodes/test.m
+++ bytecodes/test.m
@@ -1,1 +1,113 @@
-Anadu 6
+push 0
+push 1
+push 2
+  push 3
+                   pall    
+push 4
+    push 5    
+      push    6        
+pall
+push 0 Push 0 onto the stack
+push 1 Push 1 onto the stack
+
+push 2
+  push 3
+                   pall    
+
+
+                           
+push 4
+
+    push 5    
+      push    6        
+
+push 1
+push 2
+push 3
+pall
+
+push 1
+pint
+push 2
+pint
+push 3
+pint
+
+push 1
+push 2
+push 3
+pall
+pop
+pall
+pop
+pall
+pop
+pall
+
+push 1
+push 2
+push 3
+pall
+swap
+pall
+
+push 1
+push 2
+push 3
+pall
+add
+pall
+
+push 1
+push 2
+push 10
+push 3
+sub
+pall
+
+push 72
+pchar
+
+push 1
+push 2
+push 3
+push 4
+push 0
+push 110
+push 0
+push 108
+push 111
+push 111
+push 104
+push 99
+push 83
+pstr
+
+push 1
+push 2
+push 3
+push 4
+push 5
+push 6
+push 7
+push 8
+push 9
+push 0
+pall
+rotl
+pall
+
+
+push 1
+push 2
+push 3
+pall
+                push                              4
+push 5
+push 6
+pall
+add
+pall
+push 11111
+add
+pall
